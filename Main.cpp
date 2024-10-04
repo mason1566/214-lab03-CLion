701ade750a6ae631d65882ed0cf45aa8722a4fcb
Mason <22926064+mason1566@users.noreply.github.com>
2024-10-03 18:23:42 -0700

    Prepare for Demo

--- Main.cpp
+++ Main.cpp
@@ -4,13 +4,27 @@
 int main() {
     srand( static_cast<unsigned int>(time(0))); // set random seed
 
+    // Task 1
+    for (int i = 0; i < 3; i++) {
+        getPickCountNeededForFourSuits((true));
+    }
+
+    // Task 2
     const int ITER_COUNT = 10000000; // The count of tests to conduct
 
     int totalCardsDrawn = 0;
     for (int i = 0; i < ITER_COUNT; i++) {
+        totalCardsDrawn += getPickCountNeededForFourSuits(false, false);
+    }
+    std::cout << "Average cards drawn: " << (float) totalCardsDrawn / ITER_COUNT << std::endl;
+
+    // Task 3
+    totalCardsDrawn = 0;
+    for (int i = 0; i < ITER_COUNT; i++) {
         totalCardsDrawn += getPickCountNeededForFourSuits(false, true);
     }
     std::cout << "Average cards drawn: " << (float) totalCardsDrawn / ITER_COUNT << std::endl;
+
 
     return 0;
 }
